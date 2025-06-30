9b580c47d160703a7193fd40b7628f434353466b
KreativeThinker <sagely2005eagle@gmail.com>
2025-06-30 16:43:11 +0530

    chore: consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Anumeya/blog";
+export const SITE_DESCRIPTION =
+  "My blog about software development, projects, half-baked ideas, and more.";
