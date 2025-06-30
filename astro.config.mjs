e4eb5274bf52604882cf8d5b5106a3e2e192b84a
KreativeThinker <sagely2005eagle@gmail.com>
2025-06-30 18:42:40 +0530

    ci: add deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
+import github from '@astrojs/github'
 import mdx from '@astrojs/mdx'
 
-import tailwindcss from '@tailwindcss/vite'
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://blog.anumeya.com',
-  integrations: [mdx(), sitemap()],
-
+  integrations: [mdx(), sitemap(), github()],
   vite: {
     plugins: [tailwindcss()],
   },
